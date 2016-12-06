6ca5dd2a893b9defda9bed78da81cbc087347905
Umang Saxena <umang.saxena@sjsu.edu>
2016-12-05 23:50:44 -0800

    Working app

--- app/src/main/java/com/example/techmint/myapplication/HttpGetDemo.java
+++ app/src/main/java/com/example/techmint/myapplication/HttpGetDemo.java
@@ -7,11 +7,21 @@ package com.example.techmint.myapplication;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -33,14 +43,41 @@ public class HttpGetDemo extends AsyncTask<String, Void, String> {
     {
         String url = params[0];
         BufferedReader inStream = null;
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpRequest = new HttpPost(url);
+
+        JSONObject json = new JSONObject(); //to store POST body
+        String message;
+
         try {
-            HttpClient httpClient = new DefaultHttpClient();
-            HttpGet httpRequest = new HttpGet(url);
+            json.put("username", CommonUtilities.email);
+            json.put("studentid", CommonUtilities.studentid);
+            json.put("name", CommonUtilities.name);
+            json.put("mac_adr", CommonUtilities.MAC_ADDRESS);
+            json.put("isLogin", CommonUtilities.isLogin);
+            json.put("password", CommonUtilities.password);
+            json.put("from", CommonUtilities.getDate(CommonUtilities.from, "dd MMM yyyy", "dd/MM/yyyy"));
+            json.put("to", CommonUtilities.getDate(CommonUtilities.to, "dd MMM yyyy", "dd/MM/yyyy"));
+        } catch (Exception ex) {
+            ex.printStackTrace();;
+        }
+
+        try {
+            message = json.toString();
+            httpRequest.setEntity(new StringEntity(message, "UTF-8"));
+            httpRequest.setHeader(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
             HttpResponse response = httpClient.execute(httpRequest);
+
+            /*
+            if(!(response.getStatusLine().getStatusCode()>=200 && response.getStatusLine().getStatusCode()<300))
+            {
+
+            }
+            */
+
             inStream = new BufferedReader(
                     new InputStreamReader(
                             response.getEntity().getContent()));
-
             StringBuffer buffer = new StringBuffer("");
             String line = "";
             String NL = System.getProperty("line.separator");
@@ -64,9 +101,29 @@ public class HttpGetDemo extends AsyncTask<String, Void, String> {
         return result;
     }
 
-    protected void onPostExecute(String page)
+    protected void onPostExecute(String responseStr)
     {
-        CommonUtilities.loginResponse = page;
-        context.startActivity(new Intent(context, Dashboard.class));
+     try {
+         JSONObject a = new JSONObject(responseStr);
+         CommonUtilities.attendance = a.getJSONArray("attendance");
+         if(a.getString("isLoginFailed").equals("true"))
+         {
+             Method method = context.getClass().getMethod("loginFailed", Object.class);
+             method.invoke(context, context);
+         }
+         else
+         {
+             context.startActivity(new Intent(context, Dashboard.class));
+         }
+     }
+     catch (JSONException e) {
+         e.printStackTrace();
+     } catch (NoSuchMethodException e) {
+         e.printStackTrace();
+     } catch (IllegalAccessException e) {
+         e.printStackTrace();
+     } catch (InvocationTargetException e) {
+         e.printStackTrace();
+     }
     }
 }
--- app/src/main/java/com/example/techmint/myapplication/MainActivity.java
+++ app/src/main/java/com/example/techmint/myapplication/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.techmint.myapplication;
 
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText etStudentId;
+    EditText etPassword;
+    TextView loginFailed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        WifiManager wm = (WifiManager) getSystemService(android.content.Context.WIFI_SERVICE);
+        String mac_adr = wm.getConnectionInfo().getMacAddress();
+        CommonUtilities.MAC_ADDRESS = "77777";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String response = CommonUtilities.loginResponse;
+        loginFailed = (TextView) findViewById(R.id.loginFailed);
+        loginFailed.setVisibility(View.INVISIBLE);
     }
 
     @Override
@@ -19,10 +29,26 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
     }
 
-    public void btnClick(View view) {
-        HttpGetDemo task = new HttpGetDemo(MainActivity.this);
-        task.execute("http://www.google.com");
+    public static void loginFailed(Object arg)
+    {
+        MainActivity b = (MainActivity) arg;
+        b.findViewById(R.id.loginFailed).setVisibility(View.VISIBLE);
     }
+
+    public void btnClick(View view) {
+        etStudentId = (EditText) findViewById(R.id.email);
+        etPassword = (EditText) findViewById(R.id.password);
+
+        //CommonUtilities.password = etPassword.getText().toString();
+        //CommonUtilities.studentid = etStudentId.getText().toString();
+        CommonUtilities.isLogin = "true";
+        CommonUtilities.from="";
+        CommonUtilities.to="";
+
+        HttpGetDemo task = new HttpGetDemo(MainActivity.this);
+        task.execute("http://35.163.159.143:5002/getStudentDetails");
+    }
+
 
     /** Called when the activity has become visible. */
     @Override
@@ -38,13 +64,15 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the activity is no longer visible. */
     @Override
-    protected void onStop() {
-        super.onStop();
-    }
+    protected void onStop() { super.onStop(); }
 
     /** Called just before the activity is destroyed. */
     @Override
     public void onDestroy() {
         super.onDestroy();
+    }
+
+    public void registerClick(View view) {
+        startActivity(new Intent(this, Register.class));
     }
 }
